8b2d19d33a3d54b913df7b1c81ca5ea8097e9fcb
rowan <37481598+rowanxshi@users.noreply.github.com>
2021-11-04 17:46:04 -0400

    minor: give example pi a default z

--- test/example_obj.jl
+++ test/example_obj.jl
@@ -1,12 +1,12 @@
 import Random
 
-function π(J::AbstractArray{Bool}, z::Float64, (C, var, scdca)::Tuple{Int, Vector{Float64}, Bool})
+function π(J::AbstractArray{Bool}, z::Real, (C, var, scdca)::Tuple{Int, Vector{<: Real}, Bool})
 	δ = scdca ? 0.25 : 1.1
 	f = range(0.1, length = C, step = 0.1)
 	
 	profits = z*sum(c -> J[c]*var[c], 1:C)^δ - sum(c -> J[c]*f[c], 1:C)
 end
-function zero_D_j_π(j::Integer, J::AbstractArray{Bool}, (C, var, scdca)::Tuple{Int, Vector{Float64}, Bool})
+function zero_D_j_π(j::Integer, J::AbstractArray{Bool}, (C, var, scdca)::Tuple{Int, Vector{<: Real}, Bool})
 	δ = scdca ? 0.25 : 1.1
 	f = range(0.1, length = C, step = 0.1)
 	
@@ -18,17 +18,20 @@ function zero_D_j_π(j::Integer, J::AbstractArray{Bool}, (C, var, scdca)::Tuple{
 	J[j] = bool_j
 	z = f[j]/z
 end
-function equalise_π((J1, J2)::NTuple{2, AbstractVector{Bool}}, (C, var, scdca)::Tuple{Int, Vector{Float64}, Bool})
+function equalise_π((J1, J2)::NTuple{2, AbstractVector{Bool}}, (C, var, scdca)::Tuple{Int, Vector{<: Real}, Bool})
 	δ = scdca ? 0.25 : 1.1
 	f = range(0.1, length = C, step = 0.1)
 	
 	z = sum(c -> J1[c]*var[c], 1:C)^δ - sum(c -> J2[c]*var[c], 1:C)^δ
 	z = sum(c -> (J1[c] - J2[c])*f[c], 1:C)/z
 end
-function initiate(C::Int = 10, scdca::Bool = false; seed::Int = 10)
+function initiate(C::Int = 10, scdca::Bool = false; seed::Int = 10, z0::Real = 1)
 	Random.seed!(seed)
 	var = rand(C)
 	π_params = (C, var, scdca)
-	functions = (π = ((J, z) -> π(J, z, π_params)), equalise_π = (pair -> equalise_π(pair, π_params)), zero_D_j_π = ((j, J) -> zero_D_j_π(j, J, π_params)))
+	
+	π_example(J, z::Real = z0) = π(J, z, π_params)
+	
+	functions = (π_example, equalise_π = (pair -> equalise_π(pair, π_params)), zero_D_j_π = ((j, J) -> zero_D_j_π(j, J, π_params)))
 	return functions, π_params
 end
