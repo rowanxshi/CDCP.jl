b6242525b6bdf4219b93b1bbbc2840669008b2f4
rowan <37481598+rowanxshi@users.noreply.github.com>
2022-05-27 19:21:13 -0400

    silly test

--- src/policy/solvers.jl
+++ src/policy/solvers.jl
@@ -8,30 +8,31 @@ The solver can optionally take `D_j_obj(j, J, z)` and `zero_D_j_obj(j, J, l, r)`
 See also: [`solve!`](@ref), [`solve`](@ref)
 """
 function policy(C::Integer; scdca::Bool, obj, equalise_obj, D_j_obj = D_j(obj), zero_D_j_obj = zero_D_j(equalise_obj, falses(C)), show_time::Bool = false, emptyset = falses(C))
-	int = interval(_containers(C), -Inf, Inf)
-	working = [int; ]
-	converged = similar(working)
-	done = similar(working)
-	empty!(converged)
-	empty!(done)
-	cdcp = (; scdca, obj, D_j_obj, zero_D_j_obj, equalise_obj, emptyset)
+	#int = interval(_containers(C), -Inf, Inf)
+	#working = [int; ]
+	#converged = similar(working)
+	#done = similar(working)
+	#empty!(converged)
+	#empty!(done)
+	#cdcp = (; scdca, obj, D_j_obj, zero_D_j_obj, equalise_obj, emptyset)
 	
-	# initial converge
-	t1 = @elapsed converge!(working, converged; cdcp...)
-	show_time && println("initial converge: ", t1, "\n")
+	## initial converge
+	#t1 = @elapsed converge!(working, converged; cdcp...)
+	#show_time && println("initial converge: ", t1, "\n")
 	
-	# branch if necessary: local optimal stored in done
-	t2 = @elapsed converge_branches!(working, converged, done; cdcp...)
-	show_time && println("branching: ", t2, "\n")
+	## branch if necessary: local optimal stored in done
+	#t2 = @elapsed converge_branches!(working, converged, done; cdcp...)
+	#show_time && println("branching: ", t2, "\n")
 	
-	# brute force among local optima; final policy function stored in converged
-	policies = Vector{Union{Nothing, BitVector}}(nothing, 1)
-	cutoffs = Float64[-Inf, Inf]
-	policy = (cutoffs, policies)
+	## brute force among local optima; final policy function stored in converged
+	#policies = Vector{Union{Nothing, BitVector}}(nothing, 1)
+	#cutoffs = Float64[-Inf, Inf]
+	#policy = (cutoffs, policies)
 	
-	t3 = @timed brute!(policy, working, converged, done; cdcp...)
-	show_time && println("brute forcing: ", t3)
+	#t3 = @timed brute!(policy, working, converged, done; cdcp...)
+	#show_time && println("brute forcing: ", t3)
 	
-	any(isnothing, policies) && @warn("Some intervals do not have associated policies.")
-	policy
+	#any(isnothing, policies) && @warn("Some intervals do not have associated policies.")
+	#policy
+	1
 end
