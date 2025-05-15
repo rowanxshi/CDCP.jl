9c1bdb3e148b0659e4b937c5043aa4fc23c20a6c
rowan <37481598+rowanxshi@users.noreply.github.com>
2025-05-15 16:52:22 -0400

    warning message

--- src/CDCP.jl
+++ src/CDCP.jl
@@ -45,4 +45,8 @@ include("04compat/00structs.jl")
 include("04compat/01interface.jl")
 include("04compat/02helpers.jl")
 
+function __init__()
+	"\n\tYou have updated to CDCP.jl version 1.\n\tIf something has broken, consider reverting back to CDCP.jl version 0." |> println
+end
+
 end
